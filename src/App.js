a240c40c6a1e46c5807d75e7c7b8607fe1d0ef5a
Park Sang Je <parksj2480@naver.com>
2021-10-10 18:56:28 +0900

    2.4.3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 
 function App() {
+  const name = '리액트';
   return (
     <div>
-    <h1>리액트 안녕!</h1>
-    <h2>잘 작동하니?</h2>
+      {name === '리액트' ? (
+        <h1>리액트입니다.</h1>
+      ) : (
+        <h2>리액트가 아닙니다.</h2>
+      )}
     </div>
-
   );
 }
 
