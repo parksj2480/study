39595893a3abae4bd38f290e50b98e1816e35e96
박상제 <parksj2480@naver.com>
2021-11-11 23:31:01 +0900

    8.2.2,3

--- src/Info.js
+++ src/Info.js
@@ -4,12 +4,14 @@ const Info = () => {
   const [name, setName] = useState('');
   const [nickname, setNickname] = useState('');
   useEffect(() => {
-    console.log('렌더링이 완료되었습니다!');
-    console.log({
-      name,
-      nickname
-    });
-  });
+    console.log('effect');
+    console.log(name);
+    return () => {
+      console.log('cleanup');
+      console.log(name);
+    };
+}, [name]);
+ 
  
   const onChangeName = e => {
     setName(e.target.value);
