1f24f0ea9f6fef269a475aa3528b2baba31fc006
박상제 <parksj2480@naver.com>
2021-11-12 03:16:03 +0900

    11.1

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,22 +3,26 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
  
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+ 
 const App = () => {
+  const [todos, setTodos] = useState(createBulkTodos);
+ 
+  // 고윳값으로 사용될 id
+  // ref를 사용하여 변수 담기
+  const nextId = useRef(2501);
+ 
   (...)
- 
-  const onRemove = useCallback(
-    id => {
-      setTodos(todos.filter(todo => todo.id != = id));
-    },
-    [todos],
-  );
- 
-  return (
-    <TodoTemplate>
-      <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} onRemove={onRemove} />
-    </TodoTemplate>
-  );
 };
  
 export default App;
