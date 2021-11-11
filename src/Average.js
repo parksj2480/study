f30e51b6e0df472ea76e3859feb3fcd6f725a8b7
박상제 <parksj2480@naver.com>
2021-11-11 23:33:42 +0900

    8.5

--- src/Average.js
+++ src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState, useMemo, useCallback } from 'react';
  
 const getAverage = numbers => {
   console.log('평균값 계산 중..');
@@ -11,18 +11,20 @@ const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState('');
  
-  const onChange = e => {
+  const onChange = useCallback(e => {
     setNumber(e.target.value);
-  };
-  const onInsert = e => {
+}, []); // 컴포넌트가 처음 렌더링될 때만 함수 생성
+const onInsert = useCallback(() => {
     const nextList = list.concat(parseInt(number));
     setList(nextList);
     setNumber('');
-  };
+  }, [number, list]); // number 혹은 list가 바뀌었을 때만 함수 생성
+ 
+  const avg = useMemo(() => getAverage(list), [list]);
  
   return (
     <div>
-      <input value={number} onChange={onChange} />
+      <input value={number} onChange={onChange}  />
       <button onClick={onInsert}>등록</button>
       <ul>
         {list.map((value, index) => (
@@ -30,7 +32,7 @@ const Average = () => {
         ))}
       </ul>
       <div>
-        <b>평균값:</b> {getAverage(list)}
+        <b>평균값:</b> {avg}
       </div>
     </div>
   );
