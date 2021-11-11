2aa9c17b79f092c1449f704ff4e5424396d3fa00
박상제 <parksj2480@naver.com>
2021-11-11 21:32:27 +0900

    3.3.5

--- src/Mycomponent.js
+++ src/Mycomponent.js
@@ -1,5 +1,12 @@
 const MyComponent = props => {
-    return <div>안녕하세요, 제 이름은 {props.name}입니다.</div>;
+    const { name, children } = props;
+    return (
+      <div>
+        안녕하세요, 제 이름은 {name}입니다. <br />
+        children 값은 {children}
+        입니다.
+      </div>
+    );
   };
    
   MyComponent.defaultProps = {
