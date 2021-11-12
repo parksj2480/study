74841d63fc7b84184511a9b01f1f85e6cc59936b
박상제 <parksj2480@naver.com>
2021-11-12 17:44:18 +0900

    27.3

--- ch27/blog/blog-frontend/src/App.js
+++ ch27/blog/blog-frontend/src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
 import { Route } from 'react-router-dom';
 import PostListPage from './pages/PostListPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import WritePage from './pages/WritePage';
 import PostPage from './pages/PostPage';
+import { Helmet } from 'react-helmet-async';
 
 const App = () => {
   return (
     <>
+      <Helmet>
+        <title>REACTERS</title>
+      </Helmet>
       <Route component={PostListPage} path={['/@:username', '/']} exact />
       <Route component={LoginPage} path="/login" />
       <Route component={RegisterPage} path="/register" />
@@ -17,4 +20,5 @@ const App = () => {
     </>
   );
 };
+
 export default App;
