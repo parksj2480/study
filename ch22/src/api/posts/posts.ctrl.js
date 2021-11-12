a4486a33ec5ba8ee2cf267e9f87f4ba8d6170c54
박상제 <parksj2480@naver.com>
2021-11-12 14:09:43 +0900

    22.8

--- ch22/src/api/posts/posts.ctrl.js
+++ ch22/src/api/posts/posts.ctrl.js
@@ -61,3 +61,41 @@ export const read = async ctx => {
       ctx.throw(500, e);
     }
   };
+
+  /*
+  DELETE /api/posts/:id
+*/
+export const remove = async ctx => {
+    const { id } = ctx.params;
+    try {
+      await Post.findByIdAndRemove(id).exec();
+      ctx.status = 204; // No Content (성공하기는 했지만 응답할 데이터는 없음)
+    } catch (e) {
+      ctx.throw(500, e);
+    }
+  };
+
+  /*
+  PATCH /api/posts/:id
+  {
+    title: '수정',
+    body: '수정 내용',
+    tags: ['수정', '태그']
+  }
+*/
+export const update = async ctx => {
+    const { id } = ctx.params;
+    try {
+      const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+        new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
+        // false일 때는 업데이트되기 전의 데이터를 반환합니다.
+      }).exec();
+      if (!post) {
+        ctx.status = 404;
+        return;
+      }
+      ctx.body = post;
+    } catch (e) {
+      ctx.throw(500, e);
+    }
+  };
