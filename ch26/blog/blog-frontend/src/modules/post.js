e5a551478d62725e020366d3deeefd096ef0b309
박상제 <parksj2480@naver.com>
2021-11-12 17:05:11 +0900

    26.2.2

--- ch26/blog/blog-frontend/src/modules/post.js
+++ ch26/blog/blog-frontend/src/modules/post.js
@@ -6,38 +6,38 @@ import * as postsAPI from '../lib/api/posts';
 import { takeLatest } from 'redux-saga/effects';
 
 const [
-  READ_POST,
-  READ_POST_SUCCESS,
-  READ_POST_FAILURE,
-] = createRequestActionTypes('post/READ_POST');
-const UNLOAD_POST = 'post/UNLOAD_POST'; // 포스트 페이지에서 벗어날 때 데이터 비우기
+  LIST_POSTS,
+  LIST_POSTS_SUCCESS,
+  LIST_POSTS_FAILURE,
+] = createRequestActionTypes('posts/LIST_POSTS');
 
-export const readPost = createAction(READ_POST, id => id);
-export const unloadPost = createAction(UNLOAD_POST);
+export const listPosts = createAction(
+  LIST_POSTS,
+  ({ tag, username, page }) => ({ tag, username, page }),
+);
 
-const readPostSaga = createRequestSaga(READ_POST, postsAPI.readPost);
-export function* postSaga() {
-  yield takeLatest(READ_POST, readPostSaga);
+const listPostsSaga = createRequestSaga(LIST_POSTS, postsAPI.listPosts);
+export function* postsSaga() {
+  yield takeLatest(LIST_POSTS, listPostsSaga);
 }
 
 const initialState = {
-  post: null,
+  posts: null,
   error: null,
 };
 
-const post = handleActions(
+const posts = handleActions(
   {
-    [READ_POST_SUCCESS]: (state, { payload: post }) => ({
+    [LIST_POSTS_SUCCESS]: (state, { payload: posts }) => ({
       ...state,
-      post,
+      posts,
     }),
-    [READ_POST_FAILURE]: (state, { payload: error }) => ({
+    [LIST_POSTS_FAILURE]: (state, { payload: error }) => ({
       ...state,
       error,
     }),
-    [UNLOAD_POST]: () => initialState,
   },
   initialState,
 );
 
-export default post;
+export default posts;
