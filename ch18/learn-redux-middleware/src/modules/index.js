10eb2cfe5bfd0e68b252aa9f25324b62ead08b12
박상제 <parksj2480@naver.com>
2021-11-12 11:14:30 +0900

    18.3.1.4

--- ch18/learn-redux-middleware/src/modules/index.js
+++ ch18/learn-redux-middleware/src/modules/index.js
@@ -1,19 +1,10 @@
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import './index.css';
-import App from './App';
-import rootReducer from './modules';
-// import loggerMiddleware from './lib/loggerMiddleware';
-import { createLogger } from 'redux-logger';
-import ReduxThunk from 'redux-thunk';
+import { combineReducers } from 'redux';
+import counter from './counter';
+import sample from './sample';
 
-const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
+const rootReducer = combineReducers({
+  counter,
+  sample
+});
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+export default rootReducer;
