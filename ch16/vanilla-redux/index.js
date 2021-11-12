050e5256b46caaf7ce77a94c9df3d7462e53d126
박상제 <parksj2480@naver.com>
2021-11-12 10:30:49 +0900

    16.2.5

--- ch16/vanilla-redux/index.js
+++ ch16/vanilla-redux/index.js
@@ -1,6 +1,6 @@
 const divToggle = document.querySelector('.toggle');
 const counter = document.querySelector('h1');
-const btnIncrease = document.querySelector('#id');
+const btnIncrease = document.querySelector('#increase');
 const btnDecrease = document.querySelector('#decrease');
 
 const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
@@ -10,3 +10,8 @@ const DECREASE = 'DECREASE';
 const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
 const increase = difference => ({ type: INCREASE, difference });
 const decrease = () => ({ type: DECREASE });
+
+const initialState = {
+  toggle: false,
+  counter: 0
+};
