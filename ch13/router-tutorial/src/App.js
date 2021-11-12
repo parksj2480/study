caad749a16b70a1752483eb1c630009dae24c3e4
박상제 <parksj2480@naver.com>
2021-11-12 09:38:57 +0900

    13.2.5

--- ch13/router-tutorial/src/App.js
+++ ch13/router-tutorial/src/App.js
@@ -1,11 +1,20 @@
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
  
 const App = () => {
   return (
     <div>
-      <Route path="/" component={Home} />
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route path="/" component={Home} exact={true} />
       <Route path="/about" component={About} />
     </div>
   );
