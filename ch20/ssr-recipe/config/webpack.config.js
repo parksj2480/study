eadd8d244558f2eaab7758f6934b8034bc47d19a
박상제 <parksj2480@naver.com>
2021-11-12 13:20:07 +0900

    20.5.2

--- ch20/ssr-recipe/config/webpack.config.js
+++ ch20/ssr-recipe/config/webpack.config.js
@@ -26,7 +26,7 @@ const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const ForkTsCheckerWebpackPlugin = require('react-dev-utils/ForkTsCheckerWebpackPlugin');
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-
+const LoadablePlugin = require('@loadable/webpack-plugin');
 const postcssNormalize = require('postcss-normalize');
 
 const appPackageJson = require(paths.appPackageJson);
@@ -557,6 +557,9 @@ module.exports = function (webpackEnv) {
       ],
     },
     plugins: [
+      new LoadablePlugin(),
+      // Generates an `index.html` file with the <script> injected.
+      new HTMLWebpackPlugin(
       // Generates an `index.html` file with the <script> injected.
       new HtmlWebpackPlugin(
         Object.assign(
