c5bcbdfcd18cbb6181cfb8342e2cbcb28d12b0fd
박상제 <parksj2480@naver.com>
2021-11-12 11:10:22 +0900

    18.2.1

--- ch18/learn-redux-middleware/src/modules/index.js
+++ ch18/learn-redux-middleware/src/modules/index.js
@@ -1,11 +1,12 @@
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import rootReducer from './modules';
+import loggerMiddleware from './lib/loggerMiddleware';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 ReactDOM.render(
   <Provider store={store}>
