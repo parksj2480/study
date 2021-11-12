cb08bb480c0add862f3c44a4b2bc9a8d03556937
박상제 <parksj2480@naver.com>
2021-11-12 13:12:18 +0900

    20.4.5

--- ch20/ssr-recipe/src/index.server.js
+++ ch20/ssr-recipe/src/index.server.js
@@ -19,35 +19,36 @@ const chunks = Object.keys(manifest.files)
   .map(key => `<script src="${manifest.files[key]}"></script>`) // 스크립트 태그로 변환하고
   .join(''); // 합침
 
-function createPage(root) {
-  return `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <link rel="shortcut icon" href="/favicon.ico" />
-    <meta
-      name="viewport"
-      content="width=device-width,initial-scale=1,shrink-to-fit=no"
-    />
-    <meta name="theme-color" content="#000000" />
-    <title>React App</title>
-    <link href="${manifest.files['main.css']}" rel="stylesheet" />
-  </head>
-  <body>
-    <noscript>You need to enable JavaScript to run this app.</noscript>
-    <div id="root">
-      ${root}
-    </div>
-    <script src="${manifest.files['runtime-main.js']}"></script>
-    ${chunks}
-    <script src="${manifest.files['main.js']}"></script>
-  </body>
-  </html>
-    `;
-}
-const app = express();
-
-// 서버 사이드 렌더링을 처리할 핸들러 함수입니다.
+  function createPage(root, stateScript) {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,shrink-to-fit=no"
+      />
+      <meta name="theme-color" content="#000000" />
+      <title>React App</title>
+      <link href="${manifest['main.css']}" rel="stylesheet" />
+    </head>
+    <body>
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+      <div id="root">
+        ${root}
+      </div>
+      ${stateScript}
+      <script src="${manifest['runtime~main.js']}"></script>
+      ${chunks}
+      <script src="${manifest['main.js']}"></script>
+    </body>
+    </html>
+      `;
+  }
+  const app = express();
+  
+  // 서버 사이드 렌더링을 처리할 핸들러 함수입니다.
 const serverRender = (req, res, next) => {
     // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버 사이드 렌더링을 해 줍니다.
   
@@ -60,9 +61,14 @@ const serverRender = (req, res, next) => {
         </StaticRouter>
       </Provider>
     );
-    const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
-    res.send(createPage(root)); // 결과물을 응답합니다.
-  };
+    const root = ReactDOMServer.renderToString(jsx); // 렌더링을 합니다.
+  // JSON을 문자열로 변환하고 악성 스크립트가 실행되는 것을 방지하기 위해 <를 치환 처리
+  // https://redux.js.org/recipes/server-rendering#security-considerations
+  const stateString = JSON.stringify(store.getState()).replace(/</g, '\\u003c');
+  const stateScript = `<script>__PRELOADED_STATE__ = ${stateString}</script>`; // 리덕스 초기 상태를 스크립트로 주입합니다.
+
+  res.send(createPage(root, stateScript)); // 결과물을 응답합니다.
+};
   
 
 const serve = express.static(path.resolve('./build'), {
