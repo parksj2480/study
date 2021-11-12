5b0fd565c15948e0263d8bc04db27617decb4ea5
박상제 <parksj2480@naver.com>
2021-11-12 13:10:01 +0900

    20.4.4

--- ch20/ssr-recipe/src/index.server.js
+++ ch20/ssr-recipe/src/index.server.js
@@ -4,6 +4,10 @@ import { StaticRouter } from 'react-router-dom';
 import App from './App';
 import path from 'path';
 import fs from 'fs';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './modules';
 
 // asset-manifest.json에서 파일 경로들을 조회합니다.
 const manifest = JSON.parse(
@@ -45,17 +49,21 @@ const app = express();
 
 // 서버 사이드 렌더링을 처리할 핸들러 함수입니다.
 const serverRender = (req, res, next) => {
-  // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버 사이드 렌더링을 해 줍니다.
-
-  const context = {};
-  const jsx = (
-    <StaticRouter location={req.url} context={context}>
-      <App />
-    </StaticRouter>
-  );
-  const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
-  res.send(createPage(root)); // 결과물을 응답합니다.
-};
+    // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버 사이드 렌더링을 해 줍니다.
+  
+    const context = {};
+    const store = createStore(rootReducer, applyMiddleware(thunk));
+    const jsx = (
+      <Provider store={store}>
+        <StaticRouter location={req.url} context={context}>
+          <App />
+        </StaticRouter>
+      </Provider>
+    );
+    const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
+    res.send(createPage(root)); // 결과물을 응답합니다.
+  };
+  
 
 const serve = express.static(path.resolve('./build'), {
   index: false // "/" 경로에서 index.html을 보여 주지 않도록 설정
