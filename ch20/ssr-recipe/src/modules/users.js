b1ea39223dd27e324431275633f3057ea781864f
박상제 <parksj2480@naver.com>
2021-11-12 13:13:32 +0900

    20.4.6

--- ch20/ssr-recipe/src/modules/users.js
+++ ch20/ssr-recipe/src/modules/users.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
+import { call, put, takeEvery } from 'redux-saga/effects';
 
 const GET_USERS_PENDING = 'users/GET_USERS_PENDING';
 const GET_USERS_SUCCESS = 'users/GET_USERS_SUCCESS';
 const GET_USERS_FAILURE = 'users/GET_USERS_FAILURE';
+
+const GET_USER = 'users/GET_USER';
+const GET_USER_SUCCESS = 'users/GET_USER_SUCCESS';
+const GET_USER_FAILURE = 'users/GET_USER_FAILURE';
 
 const getUsersPending = () => ({ type: GET_USERS_PENDING });
 const getUsersSuccess = payload => ({ type: GET_USERS_SUCCESS, payload });
@@ -12,18 +17,33 @@ const getUsersFailure = payload => ({
   payload
 });
 
+export const getUser = id => ({ type: GET_USER, payload: id });
+const getUserSuccess = data => ({ type: GET_USER_SUCCESS, payload: data });
+const getUserFailure = error => ({
+  type: GET_USER_FAILURE,
+  payload: error,
+  error: true
+});
+
 export const getUsers = () => async dispatch => {
-  try {
-    dispatch(getUsersPending());
-    const response = await axios.get(
-      'https://jsonplaceholder.typicode.com/users'
-    );
-    dispatch(getUsersSuccess(response));
-  } catch (e) {
-    dispatch(getUsersFailure(e));
-    throw e;
-  }
+  (...)
 };
+
+const getUserById = id =>
+  axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+function* getUserSaga(action) {
+  try {
+    const response = yield call(getUserById, action.payload);
+    yield put(getUserSuccess(response.data));
+  } catch (e) {
+    yield put(getUserFailure(e));
+  }
+}
+
+export function* usersSaga() {
+  yield takeEvery(GET_USER, getUserSaga);
+}
 
 const initialState = {
   users: null,
@@ -40,19 +60,24 @@ const initialState = {
 
 function users(state = initialState, action) {
   switch (action.type) {
-    case GET_USERS_PENDING:
-      return { ...state, loading: { ...state.loading, users: true } };
-    case GET_USERS_SUCCESS:
+    (...)
+    case GET_USER:
       return {
         ...state,
-        loading: { ...state.loading, users: false },
-        users: action.payload.data
+        loading: { ...state.loading, user: true },
+        error: { ...state.error, user: null }
       };
-    case GET_USERS_FAILURE:
+    case GET_USER_SUCCESS:
       return {
         ...state,
-        loading: { ...state.loading, users: false },
-        error: { ...state.error, users: action.payload }
+        loading: { ...state.loading, user: false },
+        user: action.payload
+      };
+    case GET_USER_FAILURE:
+      return {
+        ...state,
+        loading: { ...state.loading, user: false },
+        error: { ...state.error, user: action.payload }
       };
     default:
       return state;
