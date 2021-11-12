9cddfa49e8017eb52f48c203dadd9684cd708dac
박상제 <parksj2480@naver.com>
2021-11-12 10:33:30 +0900

    16.2.9

--- ch16/vanilla-redux/index.js
+++ ch16/vanilla-redux/index.js
@@ -5,7 +5,21 @@ const counter = document.querySelector('h1');
 const btnIncrease = document.querySelector('#id');
 const btnDecrease = document.querySelector('#decrease');
 const store = createStore(reducer);
-
+const render = () => {
+    const state = store.getState(); // 현재 상태를 불러옵니다.
+    // 토글 처리
+    if (state.toggle) {
+      divToggle.classList.add('active');
+    } else {
+      divToggle.classList.remove('active');
+    }
+    // 카운터 처리
+    counter.innerText = state.counter;
+  };
+  
+  render();
+  store.subscribe(render);
+  
 const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
 const INCREASE = 'INCREASE';
 const DECREASE = 'DECREASE';
