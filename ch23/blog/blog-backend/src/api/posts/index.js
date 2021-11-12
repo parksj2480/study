dbf5110e8cf02abebc8623103b440dd0c213876b
박상제 <parksj2480@naver.com>
2021-11-12 14:57:19 +0900

    23.4-6

--- ch23/blog/blog-backend/src/api/posts/index.js
+++ ch23/blog/blog-backend/src/api/posts/index.js
@@ -1,18 +1,17 @@
-const Router = require('koa-router');
+import Router from 'koa-router';
+import * as postsCtrl from './posts.ctrl';
+import checkLoggedIn from '../../lib/checkLoggedIn';
+
 const posts = new Router();
 
-const printInfo = ctx => {
-  ctx.body = {
-    method: ctx.method,
-    path: ctx.path,
-    params: ctx.params,
-  };
-};
+posts.get('/', postsCtrl.list);
+posts.post('/', checkLoggedIn, postsCtrl.write);
 
-posts.get('/', printInfo);
-posts.post('/', printInfo);
-posts.get('/:id', printInfo);
-posts.delete('/:id', printInfo);
-posts.put('/:id', printInfo);
-posts.patch('/:id', printInfo);
-module.exports = posts;
+const post = new Router(); // /api/posts/:id
+post.get('/', postsCtrl.read);
+post.delete('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.remove);
+post.patch('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update);
+
+posts.use('/:id', postsCtrl.getPostById, post.routes());
+
+export default posts;
