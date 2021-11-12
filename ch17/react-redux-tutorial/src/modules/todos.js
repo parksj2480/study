9609e9fc3c8f3d5686da99552e6e7fc505b05f17
박상제 <parksj2480@naver.com>
2021-11-12 10:59:19 +0900

    17.6.2

--- ch17/react-redux-tutorial/src/modules/todos.js
+++ ch17/react-redux-tutorial/src/modules/todos.js
@@ -1,4 +1,5 @@
 import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
 
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경함
 const INSERT = 'todos/INSERT'; // 새로운 todo를 등록함
@@ -32,26 +33,31 @@ const initialState = {
       }
     ]
   };
-  
-  const todos = handleActions(
-    {
-      [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
-      [INSERT]: (state, { payload: todo }) => ({
-        ...state,
-        todos: state.todos.concat(todo),
+
+
+
+const todos = handleActions(
+  {
+    [CHANGE_INPUT]: (state, { payload: input }) =>
+      produce(state, draft => {
+        draft.input = input;
       }),
-      [TOGGLE]: (state, { payload: id }) => ({
-        ...state,
-        todos: state.todos.map(todo =>
-          todo.id = = = id ? { ...todo, done: !todo.done } : todo,
-        ),
+    [INSERT]: (state, { payload: todo }) =>
+      produce(state, draft => {
+        draft.todos.push(todo);
       }),
-      [REMOVE]: (state, { payload: id }) => ({
-        ...state,
-        todos: state.todos.filter(todo => todo.id != = id),
+    [TOGGLE]: (state, { payload: id }) =>
+      produce(state, draft => {
+        const todo = draft.todos.find(todo => todo.id = = = id);
+        todo.done = !todo.done;
       }),
-    },
-    initialState,
-  );
-  
-  export default todos;
+    [REMOVE]: (state, { payload: id }) =>
+      produce(state, draft => {
+        const index = draft.todos.findIndex(todo => todo.id = = = id);
+        draft.todos.splice(index, 1);
+      }),
+  },
+  initialState,
+);
+
+export default todos;
